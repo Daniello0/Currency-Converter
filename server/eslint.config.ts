34719a78bfab9af9e18abfa3d60212adc911680d
Daniel <danilactpax@gmail.com>
2025-08-23 15:50:08 +0300

    start refactoring to TS, refactor server-part to TS

---
+++ server/eslint.config.ts
@@ -0,0 +1,46 @@
+import * as js from '@eslint/js';
+// @ts-expect-error tsParser импортируется
+import tsParser from '@typescript-eslint/parser';
+import * as tsPlugin from '@typescript-eslint/eslint-plugin';
+import * as globals from 'globals';
+
+export default [
+    js.configs.recommended,
+
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+    },
+
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin,
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+        },
+    },
+];
---
+++ server/src/Cache.ts
@@ -0,0 +1,32 @@
+import type { NextFunction, Request, Response } from 'express';
+// @ts-expect-error mcache импортируется
+import mcache from 'memory-cache';
+
+export default class Cache {
+    static assignMemoryCache = (durationInSeconds: number) => {
+        return (req: Request, res: Response, next: NextFunction) => {
+            const key = req.url;
+            const cachedBody = mcache.get(key);
+
+            if (cachedBody) {
+                res.send(cachedBody);
+            } else {
+
+                const originalSend: Response['send'] = res.send.bind(res);
+                const originalJson: Response['json'] = res.json.bind(res);
+
+                res.send = ((body?: unknown) => {
+                    mcache.put(key, body, durationInSeconds * 1000);
+                    return originalSend(body);
+                }) as Response['send'];
+
+                res.json = ((body?: unknown) => {
+                    mcache.put(key, body, durationInSeconds * 1000);
+                    return originalJson(body as unknown);
+                }) as Response['json'];
+
+                next();
+            }
+        };
+    };
+}
